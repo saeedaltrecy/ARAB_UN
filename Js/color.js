666db5be7d00199d2f5ddbc645a1c08ef9af0c7b
saeedaltrecy <saeedsalmeenaltrecy@gmail.com>
2025-11-26 17:01:03 +0300

    first commit

---
+++ Js/color.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#009e91', // لون الروابط
+                language: '#fd823b', // لون زر اللغة الجديد
+                gradient: {
+                    start: '#009e91', // بداية التدرج
+                    end: '#fd823b'   // نهاية التدرج
+                }
+            },
+            fontFamily: {
+                'tajawal': ['Tajawal', 'sans-serif'],
+            }
+        }
+    }
+}
